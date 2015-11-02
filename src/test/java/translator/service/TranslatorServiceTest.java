5bae7232eb4ca624b120cd58a6f22064b49262c5
Marcos Ruiz García <648045@unizar.es>
2015-11-02 20:25:41 +0100

    Edit tests

--- src/test/java/translator/service/TranslatorServiceTest.java
+++ src/test/java/translator/service/TranslatorServiceTest.java
@@ -20,8 +20,8 @@ public class TranslatorServiceTest {
     TranslatorService translatorService;
     @Test
     public void translateTest() throws Exception {
-        TranslatedText translatedText = translatorService.translate("en", "es", "This is a test of translation service");
-        assertEquals("Esto es una prueba de servicio de traducción",translatedText.getTranslation());
+        TranslatedText translatedText = translatorService.translate("en", "es", "I wear red shirt and I like my trousers");
+        assertEquals("Me pongo rojo de la camisa y me gusta que mis pantalones",translatedText.getTranslation());
     }
 
 }
--- src/test/java/translator/web/ws/TranslatorEnpointTest.java
+++ src/test/java/translator/web/ws/TranslatorEnpointTest.java
@@ -42,11 +42,11 @@ public class TranslatorEnpointTest {
 		GetTranslationRequest request = new GetTranslationRequest();
 		request.setLangFrom("en");
 		request.setLangTo("es");
-		request.setText("This is a test of translation service");
+		request.setText("I wear red shirt and I like my trousers");
 		Object response = new WebServiceTemplate(marshaller).marshalSendAndReceive("http://localhost:"
 				+ port + "/ws", request);
 		assertNotNull(response);
 		assertThat(response, instanceOf(GetTranslationResponse.class));
-		assertThat(((GetTranslationResponse) response).getTranslation(), is("Esto es una prueba de servicio de traducción"));
+		assertThat(((GetTranslationResponse) response).getTranslation(), is("Me pongo rojo de la camisa y me gusta que mis pantalones"));
 	}	
 }
